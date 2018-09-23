4ef462a60509f91710c7768b4bfe56b236d943e0
DavidBarke <david.barkemeyer@gmx.de>
2018-09-23 14:10:08 +0200

    Test abgeschlossen

--- inst/test_list_of_reactives/App.R
+++ inst/test_list_of_reactives/App.R
@@ -1,12 +1,15 @@
 # Frage: Kann einem Modul eine Liste von reactiveValues übergeben werden?
 # Motivation: Übergabe von verschiedenen reactiveValues, die zum Beispiel unterschiedliche
-# Arten von Daten speichern an select_data()
+# Arten von Daten speichern an select_data
+# Ergebnis: reactiveValues müssen als Liste übergeben werden (ACHTUNG: c() funktioniert
+# nicht, da c() die Attribute entfernt. Theoretisch wäre es möglich, man müsste dann
+# jedoch anstelle von rvs$x rvs$get("x") verwenden, was unintuitiv ist.)
 
 library(shiny)
 
 module <- function(input, output, session, rvs) {
-  observe(rvs, {
-    print(rvs[[1]])
+  observe({
+    print(rvs$a$z)
   })
 }
 
@@ -15,10 +18,10 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-  rvs_1 <- reactiveValues(x = 1)
+  rvs_1 <- reactiveValues(x = 2, y = 2, z = 3)
   rvs_2 <- reactiveValues(y = 1)
 
-  callModule(module, "id_module", rvs = list(rvs_1, rvs_2))
+  callModule(module, "id_module", rvs = list(a = rvs_1, b = rvs_2))
 }
 
 shinyApp(ui, server)
